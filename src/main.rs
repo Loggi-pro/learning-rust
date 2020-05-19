6460f955e920b7334afce57861876c0620eb3133
loggi <loggi.ain@gmail.com>
2020-05-19 13:51:26 +1000

    "create struct and print it"

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+#[derive(Debug)] //for debug print
+struct Position {
+    x: i8,
+    y: i8,
+}
+
 fn main() {
-    let x = 3;
-    let y: i8 = x;
-    println!(r#"Hello {}, world!"#, x * y);
+    let pos = Position { x: 1, y: 2 };
+    println!("My position is: {{{:?}}}", pos); //or :#? for more verbose output
 }
