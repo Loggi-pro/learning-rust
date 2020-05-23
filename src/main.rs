adcc54e91030314cea878539f4a4f5fec29c0ca5
loggi <loggi.ain@gmail.com>
2020-05-24 03:28:52 +1000

    Pass struct to function as mutable reference (to change)

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ struct Position {
     y: i8,
 }
 
+fn update_y(pos: &mut Position) {
+    pos.y -= 10;
+}
+
+fn update_xy(pos: &mut Position) {
+    pos.x += 1;
+    update_y(pos); //here pos== &mut Position
+}
+
 fn main() {
     let mut pos = Position { x: 1, y: 2 };
-    let pos2 = &mut pos;
-    pos2.x = 12;
+    update_xy(&mut pos);
 
     println!("My position is: {{{:?}}}", pos); //or :#? for more verbose output
 }
