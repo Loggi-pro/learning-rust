f37f2c16bf5e31103b07e5def39d7a7f122b7131
loggi <loggi.ain@gmail.com>
2020-05-24 03:34:56 +1000

    Implement struct methods and call it

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ struct Position {
     x: i8,
     y: i8,
 }
+impl Position {
+    fn update_y(&mut self) {
+        self.y -= 10;
+    }
 
-fn update_y(pos: &mut Position) {
-    pos.y -= 10;
-}
-
-fn update_xy(pos: &mut Position) {
-    pos.x += 1;
-    update_y(pos); //here pos== &mut Position
+    fn update_xy(&mut self) {
+        self.x += 1;
+        self.update_y();
+    }
 }
 
 fn main() {
     let mut pos = Position { x: 1, y: 2 };
-    update_xy(&mut pos);
+    pos.update_xy(); //call method
+    Position::update_xy(&mut pos); //or this one: universal function call
 
     println!("My position is: {{{:?}}}", pos); //or :#? for more verbose output
 }
