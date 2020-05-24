6cbaf934d9ba9ba19d642489010f95cd03b98408
loggi <loggi.ain@gmail.com>
2020-05-24 16:11:29 +1000

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,11 @@
-extern crate sdl2;
-
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-
-use std::time::Duration;
-
-pub fn main() -> Result<(), String> /*Optional empty type or String*/ {
-    let context = sdl2::init(); //Optional Sdl or String
-    let sdl_context = match context {
-        //like exception check
-        Ok(result) => result,
-        Err(message) => {
-            println!("SDL reported error: '{}", message);
-            return Ok(()); //return empty_type from main
-        }
-    };
-    //alternative
-    //let sdl_context = context?; //auto unwind result, in error case - return from function
-
-    let video_subsystem = sdl_context.video()?;
-
-    let window = video_subsystem
-        .window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap(); //unwrap do the same as "?""
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-    'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-        // The rest of the game loop goes here...
-
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
-    return Ok(());
+#[test]
+fn funny_name() {
+    assert_eq!(1, 1);
 }
+
+#[test]
+fn funny_name2() {
+    assert_eq!(2, 34);
+}
+
+pub fn main() {}
