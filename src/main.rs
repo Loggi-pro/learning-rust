e680bb73316fc8a1e5a686417d4d8db84ad9bc3a
loggi <loggi.ain@gmail.com>
2020-05-24 16:39:47 +1000

    generic functions and lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-mod test; //add test.rs
-          //also add #[path = "foo.rs"] before to import file at path
-          //also can import a directory
+fn generic_function<Type1>(val: Type1) -> Type1 {
+    return val;
+}
+
+//get lifetime of VAL and stick it to lifetime of return value (make same lifetime)
+fn generic_function_borrow<'t, Type1>(val: &'t Type1) -> &'t Type1 {
+    return val;
+}
 
 pub fn main() {
-    let _x = test::other_function();
+    let _x = generic_function(4);
+    let _y = generic_function_borrow(&_x);
 }
