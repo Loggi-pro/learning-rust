54bde5303959965ae28b822ea23ee77524898e36
loggi <loggi.ain@gmail.com>
2020-10-11 23:49:37 +1000

    Rust book: 12.4. CLI Programm | TDD | Tests

--- src/rust_book/minigrep/mod.rs
+++ src/rust_book/minigrep/mod.rs
@@ -1,8 +1,13 @@
 mod minigrep;
 use std::env;
 use std::process;
+//this is must be main file
+
 
 //this must be main function
+
+//to check you can run: cargo run frog poem.txt
+//or cargo run body poem.txt
 #[allow(dead_code)]
 pub fn run() {
     //parse argument list
@@ -18,3 +23,4 @@ pub fn run() {
         println!("Application error: {}", e);
     }
 }
+
