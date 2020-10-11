5b2a9b145dc44456678aada236133a97d17a06c7
loggi <loggi.ain@gmail.com>
2020-10-12 00:08:29 +1000

    Rust book: 12.6. CLI Programm | eprintln | redirect errors to error output

--- src/rust_book/minigrep/mod.rs
+++ src/rust_book/minigrep/mod.rs
@@ -8,19 +8,22 @@ use std::process;
 
 //to check you can run: cargo run frog poem.txt
 //or cargo run body poem.txt
+//for case insensitive search in file: $Env:CASE_INSENSITIVE=1; cargo run to poem.txt
+//call after Remove-Item Env:CASE_INSENSITIVE to remove env var
 #[allow(dead_code)]
 pub fn run() {
     //parse argument list
     let args: Vec<String> = env::args().collect();
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
+        process::exit(1);
     }
 }
 
