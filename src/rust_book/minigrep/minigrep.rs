872fa171e8429663618f638cc79b002869bd34b8
loggi <loggi.ain@gmail.com>
2020-10-15 17:01:42 +1000

    Rust book: 13.3. Improving CLI program | rewrite with iterators and function style loggi on 10/13/2020, 2:05:38 PM

--- src/rust_book/minigrep/minigrep.rs
+++ src/rust_book/minigrep/minigrep.rs
@@ -1,6 +1,6 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 //this must be lib file
 
 pub struct Config {
@@ -9,42 +9,54 @@ pub struct Config {
     pub case_sensitive: bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err(); //is_err->env var not set->case_sens...=true
-        Ok(Config { query, filename,case_sensitive })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 #[allow(dead_code)]
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    let search_func = if config.case_sensitive { search } else { search_case_insensitive };
-    
-    for line in search_func(&config.query,&contents){
+    let search_func = if config.case_sensitive {
+        search
+    } else {
+        search_case_insensitive
+    };
+
+    for line in search_func(&config.query, &contents) {
         println!("{}", line);
     }
     Ok(())
 }
 
-pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
-    let mut results = Vec::new();
-    for line in contents.lines(){
-        if line.contains(query){
-            results.push(line);
-        }
-    }
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
-pub fn search_case_insensitive<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
-    let query  = query.to_lowercase();
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
     let mut results = Vec::new();
-    for line in contents.lines(){
-        if line.to_lowercase().contains(&query){
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -55,33 +67,36 @@ pub fn search_case_insensitive<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
 mod tests {
     use super::*;
     #[test]
-    fn one_result(){
+    fn one_result() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.";
-        assert_eq!(vec!["safe, fast, productive."],search(query,contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
-    fn case_sensitive(){
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
 Duct tape.";
-        assert_eq!(vec!["safe, fast, productive."],search(query,contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
     #[test]
-    fn case_insensitive(){
+    fn case_insensitive() {
         let query = "rUst";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
 Trust me.";
-        assert_eq!(vec!["Rust:","Trust me."],search_case_insensitive(query,contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- src/rust_book/minigrep/mod.rs
+++ src/rust_book/minigrep/mod.rs
@@ -13,8 +13,7 @@ use std::process;
 #[allow(dead_code)]
 pub fn run() {
     //parse argument list
-    let args: Vec<String> = env::args().collect();
-    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
