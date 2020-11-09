0999b781164698db1b85000ef57e850c95c76071
loggi <loggi.ain@gmail.com>
2020-11-09 17:21:26 +1000

    Rust embed 5.4: led blinking

--- src/led-roulette/src/main.rs
+++ src/led-roulette/src/main.rs
@@ -6,15 +6,17 @@ extern crate panic_halt;
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
 extern crate f3; // provides memory.x
 use cortex_m_rt::entry;
+use aux5::{prelude::*,Delay,Leds};
 
 #[entry]
 fn main()->! {
-
-    let _y;
-    let mut x = 42;
-    _y = x;
+    let (mut delay,mut leds): (Delay, Leds) = aux5::init();
+    let half_period = 500_u16;
     loop {
-        x = x+1;
+        leds[0].on();
+        delay.delay_ms(half_period);
+        leds[0].off();
+        delay.delay_ms(half_period);
     }
 }
 
